2d56ab9fdd0f794974ff0e20d324483d4f9cd86d
Ian Horsman <ianphorsman@gmail.com>
2023-04-15 19:40:58 -0400

    fix(styles): removed border gradients plugin

--- gpt-tooling/tailwind.config.ts
+++ gpt-tooling/tailwind.config.ts
@@ -3,16 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    linearBorderGradients: (theme: (arg0: string) => any) => ({
-      colors: theme('colors')
-    }),
     extend: {
       zIndex: {
         '-z-10': '-10'
       }
     },
   },
-  plugins: [
-    require('tailwindcss-border-gradients')()
-  ],
+  plugins: [],
 } satisfies Config;
