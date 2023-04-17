78af18b8a3981c83688b7364c9ab682024f75370
Ian Horsman <ianphorsman@gmail.com>
2023-04-17 02:44:56 -0400

    fix(style): added active state to message

--- gpt-tooling/src/components/Message/Message.jsx
+++ gpt-tooling/src/components/Message/Message.jsx
@@ -15,7 +15,7 @@ const Message = ({
       backgroundImage: `linear-gradient(to bottom right, ${linearGradientColor}, ${linearGradientColor}), radial-gradient(circle at top left, ${radialGradientColorFrom}, ${radialGradientColorTo})`,
       backgroundOrigin: 'border-box',
       backgroundClip: 'padding-box, border-box'
-    }} className={`px-4 py-2 rounded-md bg-blend-color-burn active:brightness-200 hover:cursor-pointer hover:brightness-150 relative hover:hue-rotate-90 transition duration-300 hover:duration-300`}>
+    }} className={`px-4 py-2 rounded-md bg-blend-color-burn active:saturate-200 active:brightness-200 active:filter hover:cursor-pointer hover:brightness-150 relative hover:hue-rotate-90 transition duration-300 hover:duration-300`}>
       <div className="px-4 py-2 rounded text-stone-400 bg-transparent">
           {text}
       </div>
