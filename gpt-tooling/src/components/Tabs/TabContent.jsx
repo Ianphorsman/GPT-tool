fa3afb5eab1ab0defebeb181662a41ab2be2cc74
Ian Horsman <ianphorsman@gmail.com>
2024-02-06 19:00:07 -0500

    fix: removed unused Tabs component

--- gpt-tooling/src/components/Tabs/TabContent.jsx
+++
@@ -1,9 +0,0 @@
-const TabContent = ({ children, isActive }) => {
-  return (
-    <div style={{ display: isActive ? 'block' : 'none' }}>
-      {children}
-    </div>
-  );
-};
-
-export default TabContent
