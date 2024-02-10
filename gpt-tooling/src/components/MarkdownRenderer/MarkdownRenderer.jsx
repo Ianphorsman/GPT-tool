0e4541e30ad8ba2994e5ddb342180cf64891d42f
Ian Horsman <ianphorsman@gmail.com>
2024-02-09 22:58:13 -0500

    fix: added basic markdown rendering for lists and tables

--- gpt-tooling/src/components/MarkdownRenderer/MarkdownRenderer.jsx
+++ gpt-tooling/src/components/MarkdownRenderer/MarkdownRenderer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Markdown from 'react-markdown'
 import rehypeHighlight from 'rehype-highlight'
+import remarkGfm from 'remark-gfm'
 
 const MarkdownRenderer = ({ children }) => {
   return (
     <Markdown
-
+      remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeHighlight]}
+      className="prose"
     >{children}</Markdown>
   )
 }
