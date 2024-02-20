cadeb35b21360eb0eb17fb56aab490d202a02de4
Ian Horsman <ianphorsman@gmail.com>
2024-02-19 19:04:22 -0500

    fix: added ability to remove agents from AgentPanel

--- gpt-tooling/src/components/AgentsPanel/AgentsPanel.jsx
+++ gpt-tooling/src/components/AgentsPanel/AgentsPanel.jsx
@@ -6,24 +6,54 @@ const AgentsPanel = ({
   agents = [],
   activeAgent,
   setActiveAgent,
-  addAgent
+  addAgent,
+  removeAgent
 }) => {
   const [view, setView] = useState('pre-built')
   return (
-    <section className="w-16 border-x-2 border-neutral flex flex-col gap-0 justify-between">
+    <section
+      className="border-x-2 border-neutral flex flex-col gap-0 justify-between"
+    >
       <div className="m-0">
         {agents.length > 0 && view === 'custom' ? (
           <Join vertical className="m-0 rounded-none">
             {agents.map(({ name, initials, id }) => (
-              <Tooltip key={id} message={name} position="right">
-                <Button
-                  shape="square"
-                  color={id === activeAgent.id ? "neutral" : "ghost"}
-                  className="m-0 join-item"
-                  onClick={() => setActiveAgent(id)}
-                >
-                  {initials}
-                </Button>
+              <Tooltip key={id} message={name} position="right" color="secondary">
+                <div key={id} className="flex flex-row">
+                  <Button
+                    shape="square"
+                    color={id === activeAgent.id ? "primary" : "ghost"}
+                    className="m-0 join-item"
+                    onClick={() => setActiveAgent(id)}
+                  >
+                    {initials}
+                  </Button>
+                  <Join vertical className="m-0 rounded-none flex flex-col">
+                    <Button
+                      shape="square"
+                      size="xs"
+                      className="join-item rounded-none m-0 hover:bg-transparent disabled:bg-transparent"
+                      color="ghost"
+                      onClick={() => removeAgent(id)}
+                      disabled={activeAgent.id === id}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                        <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z" />
+                      </svg>
+                    </Button>
+                    <Button
+                      shape="square"
+                      size="xs"
+                      className="join-item rounded-none m-0 hover:bg-transparent disabled:bg-transparent"
+                      color="ghost"
+                      disabled
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                        <path fill-rule="evenodd" d="M6.455 1.45A.5.5 0 0 1 6.952 1h2.096a.5.5 0 0 1 .497.45l.186 1.858a4.996 4.996 0 0 1 1.466.848l1.703-.769a.5.5 0 0 1 .639.206l1.047 1.814a.5.5 0 0 1-.14.656l-1.517 1.09a5.026 5.026 0 0 1 0 1.694l1.516 1.09a.5.5 0 0 1 .141.656l-1.047 1.814a.5.5 0 0 1-.639.206l-1.703-.768c-.433.36-.928.649-1.466.847l-.186 1.858a.5.5 0 0 1-.497.45H6.952a.5.5 0 0 1-.497-.45l-.186-1.858a4.993 4.993 0 0 1-1.466-.848l-1.703.769a.5.5 0 0 1-.639-.206l-1.047-1.814a.5.5 0 0 1 .14-.656l1.517-1.09a5.033 5.033 0 0 1 0-1.694l-1.516-1.09a.5.5 0 0 1-.141-.656L2.46 3.593a.5.5 0 0 1 .639-.206l1.703.769c.433-.36.928-.65 1.466-.848l.186-1.858Zm-.177 7.567-.022-.037a2 2 0 0 1 3.466-1.997l.022.037a2 2 0 0 1-3.466 1.997Z" clip-rule="evenodd" />
+                      </svg>
+                    </Button>
+                  </Join>
+                </div>
               </Tooltip>
             ))}
           </Join>
@@ -49,8 +79,8 @@ const AgentsPanel = ({
           <>
             <Tooltip message="Add Agent" position="right">
               <Button
-                shape="square"
-                className="m-0 join-item"
+                size="lg"
+                className="m-0 join-item rounded-none w-full"
                 disabled={agents.length >= 5}
                 onClick={() => addAgent(agents.length + 1, { id: agents.length + 1, name: `Agent${agents.length + 1}`, initials: `A${agents.length + 1}` })}
               >
@@ -61,8 +91,8 @@ const AgentsPanel = ({
             </Tooltip>
             <Tooltip message="Use Prebuilt Setups" position="right">
               <Button
-                shape="square"
-                className="m-0 join-item"
+                size="lg"
+                className="m-0 join-item rounded-none w-full"
                 onClick={() => setView('pre-built')}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -70,12 +100,13 @@ const AgentsPanel = ({
                 </svg>
               </Button>
             </Tooltip>
-            </>
+          </>
         ) : null}
         {view === 'pre-built' ? (
           <Tooltip message="Use Custom Setups (stay tuned)" position="right">
             <Button
               shape="square"
+              size="lg"
               className="m-0 join-item"
               onClick={() => setView('custom')}
             >
