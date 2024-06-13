9f8b1e6b1b62eec7567cd9d79c8b07b049b672e9
Ian Horsman <ianphorsman@gmail.com>
2024-06-12 20:58:14 -0400

    fix: secret not leaked

--- gpt-tooling/src/utils/qdrant/qdrantClient.js
+++ gpt-tooling/src/utils/qdrant/qdrantClient.js
@@ -2,7 +2,7 @@ import { QdrantClient } from '@qdrant/js-client-rest';
 
 const client = new QdrantClient({
     url: 'https://f224262d-9bdc-4b07-b390-ea62a7f27c7d.us-east4-0.gcp.cloud.qdrant.io:6333',
-    apiKey: 'Fl_GTLRBKkfMEUIvelVarpy0OUemYcj_pge2IZFZiTRIM9pA9XTDcw',
+    apiKey: process.env.QDRANT_API_KEY
 });
 
 export default client;
